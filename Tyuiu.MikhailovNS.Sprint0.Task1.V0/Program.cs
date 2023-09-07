a0f8b6afd0adf8add77c63e6f0535c377406f803
MikhailovNS <n.s.mikhailov@gmail.com>
2023-09-07 23:23:20 +0500

    Task1

---
+++ Tyuiu.MikhailovNS.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.MikhailovNS.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Алукард";
+            int age = 426;
+            Console.WriteLine("Здравствуй, " + name + ", " + age +"-летний старик..") ;
+            Console.ReadKey();
+        }
+    }
+}
